eaaac7c0e9bb72c66bbe5938a21958ced8fecbf0
Paulo Leite <pauloleitecosta14@gmail.com>
2021-06-29 01:36:39 -0300

    Start Project Communication MS

---
+++ communication-ms/src/main/java/br/com/pauloleitecosta/communicationms/CommunicationMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.pauloleitecosta.communicationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationMsApplication.class, args);
+	}
+
+}
---
+++ communication-ms/src/test/java/br/com/pauloleitecosta/communicationms/CommunicationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pauloleitecosta.communicationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
