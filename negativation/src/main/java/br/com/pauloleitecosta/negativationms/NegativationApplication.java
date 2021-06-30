eca8ef3fd289d9472115679dcdeb7c160c0ba6b7
Paulo Leite <pauloleitecosta14@gmail.com>
2021-06-29 22:11:01 -0300

    Start Project Negativation MS

---
+++ negativation/src/main/java/br/com/pauloleitecosta/negativationms/NegativationApplication.java
@@ -0,0 +1,13 @@
+package br.com.pauloleitecosta.negativationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegativationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegativationApplication.class, args);
+	}
+
+}
---
+++ negativation/src/test/java/br/com/pauloleitecosta/negativationms/NegativationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pauloleitecosta.negativationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegativationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
