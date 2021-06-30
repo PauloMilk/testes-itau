508e2f6f397401d5991c8bbf3b5913081b548f3f
Paulo Leite <pauloleitecosta14@gmail.com>
2021-06-29 23:13:10 -0300

    Start Project Payment MS

---
+++ paymentms/src/test/java/br/com/pauloleitecosta/paymentms/PaymentmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pauloleitecosta.paymentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
